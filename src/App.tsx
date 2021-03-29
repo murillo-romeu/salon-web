c0dc651e43736e9669ac013944703f07d9f8b648
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-29 18:02:11 -0300

    sigin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignIn from './pages/signIn';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <h1>Hello World</h1>
+    <SignIn />
   </>
 );
 
---
+++ src/pages/signIn/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logo} height={120} alt="Salon" />
+      <form>
+        <h1>Acesso o sistema.</h1>
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit"> Entrar </button>
+        <a href="forgot">Esqueci a minha senha!</a>
+      </form>
+      <a href="forgot">
+        <FiLogIn />
+        Criar conta!
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
