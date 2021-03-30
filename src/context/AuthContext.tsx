e2d49520cb8c00eb7325d6273e9d897529215451
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-30 12:20:36 -0300

    Criando hook useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { createContext, useCallback, useState } from 'react';
+import React, {
+  createContext, useCallback, useState, useContext,
+} from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -12,14 +14,14 @@ interface SignInCredentials {
   password: string;
 }
 
-interface PropsAuthContext {
+interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<PropsAuthContext>({} as PropsAuthContext);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@Salon:token');
     const user = localStorage.getItem('@Salon:user');
@@ -50,3 +52,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must bem used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
